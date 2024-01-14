8d5b5a504c78d100411bb60100c33f8d532ffca0
Zou <78598969+Zetrey@users.noreply.github.com>
2024-01-15 00:01:16 +0800

    Add files via upload

--- js/theme.js
+++ js/theme.js
@@ -1,28 +1,80 @@
-let flag1 = false;
-function moreColorShow(){
+let themecolor = 0;
+function themeColor(x = 0) {
     var box = document.getElementsByClassName('setting-box-01');
-    if (flag1 == false) {
-        flag1 = true;
-        box[0].style.height = '200px';
+    var mb = document.getElementById('cmore');
+    var cs = document.getElementsByClassName('cspan');
+    if (x == themecolor) {
+        return;
     }
-    else{
-        flag1 = false;
-        box[0].style.height = '78px';
+    else {
+        if (x < 7) {
+            cs[x].style.border = '3px solid var(--theme-negative-04)';
+            if (themecolor != 7) {
+                cs[themecolor].style.border = 'none';
+            }
+            else {
+                box[0].style.height = '78px';
+                mb.style.opacity = 1;
+            }
+            themecolor = x;
+        }
+        else {
+            box[0].style.height = '200px';
+            mb.style.opacity = 0.7;
+            cs[themecolor].style.border = 'none';
+            themecolor = x;
+        }
+        var root = document.querySelector(":root");
+        switch (x) {
+            case 0: root.style.setProperty("--h", "3");
+                root.style.setProperty("--w", "25%");
+                root.style.setProperty("--b", "5%");
+                break;
+            case 1: root.style.setProperty("--h", "24");
+                root.style.setProperty("--w", "28%");
+                root.style.setProperty("--b", "0%");
+                break;
+            case 2: root.style.setProperty("--h", "48");
+                root.style.setProperty("--w", "25%");
+                root.style.setProperty("--b", "0%");
+                break;
+            case 3: root.style.setProperty("--h", "118");
+                root.style.setProperty("--w", "27%");
+                root.style.setProperty("--b", "12%");
+                break;
+            case 4: root.style.setProperty("--h", "228");
+                root.style.setProperty("--w", "38%");
+                root.style.setProperty("--b", "0%");
+                break;
+            case 5: root.style.setProperty("--h", "275");
+                root.style.setProperty("--w", "33%");
+                root.style.setProperty("--b", "3%");
+                break;
+            case 6: root.style.setProperty("--h", "337");
+                root.style.setProperty("--w", "40%");
+                root.style.setProperty("--b", "0%");
+                break;
+            case 7:
+                break;
+        }
     }
 }
 
-let flag2 = false;
+
+
+
+let darkmod = false;
 function darkMod() {
     var bg = document.getElementsByClassName('dkmod-bt');
     var dot = document.getElementById('dkmod-bt-dot');
-    if (flag2 == false) {
-        flag2 = true;
+    if (darkmod == false) {
+        darkmod = true;
         bg[0].style['background-color'] = 'var(--theme-color-08)';
         dot.style.left = '18px';
         document.documentElement.dataset.theme = 'dark';
     }
     else{
-        flag2 = false;
+        darkmod = false;
         bg[0].style['background-color'] = 'var(--theme-gray)';
         dot.style.left = '0px';
         document.documentElement.dataset.theme = null;
