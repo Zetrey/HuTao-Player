b816261dcd6410af35af01585351d623b11342af
Zou <78598969+Zetrey@users.noreply.github.com>
2024-01-17 04:55:22 +0800

    Add files via upload

--- js/main.js
+++ js/main.js
@@ -1,13 +1,12 @@
 
 var root = document.querySelector(":root")
-const ThColor03 = getComputedStyle(root).getPropertyValue("--theme-color-03")
 
 //显示音量滑块
 function volumnShow(){
-
     var dock = document.getElementsByClassName('music-dock');
     var bar = document.getElementsByClassName('volumn-line-bar');
     var btnshader = document .getElementById('volumn-button');
+    var ThColor03 = getComputedStyle(root).getPropertyValue("--theme-color-03")
     dock[0].style['grid-template-columns'] = '162px auto 162px';
     bar[0].style.padding = '17px 17px 17px 52px';
     btnshader.style['box-shadow'] = '1px 3px 10px ' + ThColor03;
@@ -20,6 +19,22 @@ function volumnHide(){
     dock[0].style['grid-template-columns'] = '162px auto 54px';
     bar[0].style['padding'] = '17px 17px 17px 21px';
     btnshader.style['box-shadow'] = 'none';
+}
+//静音
+var sound = true;
+function soundOff(){
+    var curve1 = document.getElementById('volumn-curve1');
+    var curve2 = document.getElementById('volumn-curve2');
+    if (sound == true) {
+        curve1.style.display = 'none';
+        curve2.style.display = 'none';
+        sound = false;
+    }
+    else{
+        curve1.style.display = 'block';
+        curve2.style.display = 'block';
+        sound = true;
+    }
 }
 //展示设置面板
 function settingShow(){
--- js/theme.js
+++ js/theme.js
@@ -1,14 +1,24 @@
 var root = document.querySelector(":root");
-var themecolor = 0;
+var themecolor = 0;     //定义默认颜色
+const tip = {
+    t1: document.getElementById('cldot1'),
+    t2: document.getElementById('cldot2'),
+    t3: document.getElementById('cldot3')
+}
+const inf = {
+    i1: document.getElementById('cli1'),
+    i2: document.getElementById('cli2'),
+    i3: document.getElementById('cli3')
+}
 function themeColor(x = 0) {
-    let box = document.getElementsByClassName('setting-box-01');
-    let mb = document.getElementById('cmore');
-    let cs = document.getElementsByClassName('cspan');
-    if (x == themecolor) {
+    let box = document.getElementsByClassName('setting-box-01'),
+        mb = document.getElementById('cmore'),
+        cs = document.getElementsByClassName('cspan');
+    if (x == themecolor) {      //判断是否重复点击
         return;
     }
     else {
-        if (x < 7) {
+        if (x < 7) {    //颜色边框
             cs[x].style.border = '3px solid var(--theme-negative-04)';
             if (themecolor != 7) {
                 cs[themecolor].style.border = 'none';
@@ -19,13 +29,13 @@ function themeColor(x = 0) {
             }
             themecolor = x;
         }
-        else {
+        else {      //展开更多颜色
             box[0].style.height = '200px';
             mb.style.opacity = 0.7;
             cs[themecolor].style.border = 'none';
             themecolor = x;
         }
-        switch (x) {
+        switch (x) {        //修改主题色
             case 0: root.style.setProperty("--h", "3");
                 root.style.setProperty("--w", "25%");
                 root.style.setProperty("--b", "5%");
@@ -54,30 +64,32 @@ function themeColor(x = 0) {
                 root.style.setProperty("--w", "40%");
                 root.style.setProperty("--b", "0%");
                 break;
-            case 7:
-                let dot1 = document.getElementById('cldot1');
-                let dot2 = document.getElementById('cldot2');
-                let dot3 = document.getElementById('cldot3');
-                let h = getComputedStyle(root).getPropertyValue('--h');
-                let per1 = h / 3.6;
-                let per2 = getComputedStyle(root).getPropertyValue('--w');
-                let per3 = getComputedStyle(root).getPropertyValue('--b');
-                let i1 = document.getElementById('cli1');
-                let i2 = document.getElementById('cli2');
-                let i3 = document.getElementById('cli3');
-                dot1.style.left = `${per1}`+'%';
-                dot2.style.left = `${per2}`;
-                dot3.style.right = `${per3}`;
-                i1.innerHTML = h + 'deg';
-                i2.innerHTML = per2;
-                i3.innerHTML = per3;
+            case 7:     //点击更多颜色时修改滑块位置为当前主题色
+                var hwb = {
+                    h: getComputedStyle(root).getPropertyValue('--h'),
+                    w: getComputedStyle(root).getPropertyValue('--w'),
+                    b: getComputedStyle(root).getPropertyValue('--b')
+                }
+                let per1 = hwb.h / 3.6;
+                tip.t1.style.left = `${per1}` + '%';
+                tip.t2.style.left = `${hwb.w}`;
+                tip.t3.style.right = `${hwb.b}`;
+                infUpDate();
                 break;
         }
     }
 }
-
-
-var darkmod = false;
+function infUpDate(){   //信息更新
+    var hwb = {
+        h: getComputedStyle(root).getPropertyValue('--h'),
+        w: getComputedStyle(root).getPropertyValue('--w'),
+        b: getComputedStyle(root).getPropertyValue('--b')
+    }
+    inf.i1.innerHTML = hwb.h + 'deg';
+    inf.i2.innerHTML = hwb.w;
+    inf.i3.innerHTML = hwb.b;
+}
+var darkmod = false;    //定义当前是否为暗色模式
 function darkMod() {
     let bg = document.getElementsByClassName('dkmod-bt');
     let dot = document.getElementById('dkmod-bt-dot');
@@ -94,3 +106,75 @@ function darkMod() {
         document.documentElement.dataset.theme = null;
     }
 }
+let btico = document.getElementsByClassName('music-button-path');
+
+let boxh = document.getElementsByClassName('color-line-shaderh'),
+    boxw = document.getElementsByClassName('color-line-shaderw'),
+    boxb = document.getElementsByClassName('color-line-shaderb');
+
+function mousePos(x, element){
+    var rect = element.getBoundingClientRect();
+    var rx = x - rect.left;
+    return rx;
+}
+function elementWidth(element){
+    var rect = element.getBoundingClientRect();
+    var w = rect.width;
+    return w;
+}
+function slideDot(hwb){
+    var x = event.clientX;
+    var w = elementWidth(boxh[0]);
+    if(hwb == 'h') {
+        var px = mousePos(x, boxh[0]) - 2;
+        tip.t1.style.left = px + 'px';
+        var h1 = 360 * px / w;
+        root.style.setProperty("--h", parseInt(h1));
+        infUpDate();
+    }
+    else if(hwb == 'w') {
+        var px = mousePos(x, boxw[0]) - 2;
+        tip.t2.style.left = px + 'px';
+        var w1 = px / w * 100;
+        root.style.setProperty("--w", parseInt(w1) + '%');
+        infUpDate();
+    }
+    else{
+        var px = mousePos(x, boxb[0]) - 2;
+        document.getElementById('cldot3').style.left = px + 'px';
+        var b1 = (w - px) / w * 100;
+        root.style.setProperty("--b", parseInt(b1) + '%');
+        infUpDate();
+    }
+    var hwb = {
+        h: getComputedStyle(root).getPropertyValue('--h'),
+        w: getComputedStyle(root).getPropertyValue('--w'),
+        b: getComputedStyle(root).getPropertyValue('--b')
+    }
+    var white = parseInt(hwb.w);
+    var black = parseInt(hwb.b);
+    var adv = document.getElementById('adv1')//建议
+    if (white >= (0.6 * black) && white >= 75 && black <= 35) {
+        for (i = 0; i < 6; i++) {
+            btico[i].style.fill = '#00000066';
+        }
+        if(darkmod==false){
+            adv.style.color = '#3b3b3b';
+        }
+        else{
+            adv.style.color = 'var(--theme-bg-10)';
+        }
+    }
+    else {
+        for (i = 0; i < 6; i++) {
+            btico[i].style.fill = '#fff';
+        }
+        if (darkmod == true && black > 45) {
+            adv.style.color = '#fff';
+        }
+        else {
+            adv.style.color = 'var(--theme-bg-10)';
+        }
+    }
+}
+
